84e6028a5ee81b3142d44acbbc9f42de14a850c3
Dave <dave.soh@singtel.com>
2022-09-23 14:54:43 +0800

    Spring init

---
+++ src/main/java/com/dave/salinfo/SalinfoApplication.java
@@ -0,0 +1,13 @@
+package com.dave.salinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dave/salinfo/SalinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dave.salinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
